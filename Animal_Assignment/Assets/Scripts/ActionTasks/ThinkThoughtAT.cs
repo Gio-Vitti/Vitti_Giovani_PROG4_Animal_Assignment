aff0b080f026540cff5c56ca4bb45ff81e111a86
Giovani Vitti <giovani.mamprin@icloud.com>
2026-02-20 17:52:27 -0500

    added action task for the icon signifier

--- Animal_Assignment/Assets/Scripts/ActionTasks/ThinkThoughtAT.cs
+++ Animal_Assignment/Assets/Scripts/ActionTasks/ThinkThoughtAT.cs
@@ -1,5 +1,6 @@
 using NodeCanvas.Framework;
 using ParadoxNotion.Design;
+using Unity.VisualScripting;
 using UnityEngine;
 
 
@@ -8,10 +9,23 @@ namespace NodeCanvas.Tasks.Actions {
 	public class ThinkThoughtAT : ActionTask {
 
 		public BBParameter<GameObject> thoughtBubble;
+		public BBParameter<Transform> bubbleTransform;
+		private float time = 0;
 
-		//Use for initialization. This is called only once in the lifetime of the task.
-		//Return null if init was successfull. Return an error string otherwise
-		protected override string OnInit() {
+		private GameObject burger;
+		private GameObject light;
+		private GameObject ball;
+		private GameObject zzz;
+
+		//enable icon
+		public bool enableBurger;
+		public bool enableLight;
+		public bool enableBall;
+		public bool enableZzz;
+
+        //Use for initialization. This is called only once in the lifetime of the task.
+        //Return null if init was successfull. Return an error string otherwise
+        protected override string OnInit() {
 			return null;
 		}
 
@@ -19,13 +33,31 @@ namespace NodeCanvas.Tasks.Actions {
 		//Call EndAction() to mark the action as finished, either in success or failure.
 		//EndAction can be called from anywhere.
 		protected override void OnExecute() {
-			
-		}
+
+			//get children
+			burger = thoughtBubble.value.transform.GetChild(0).gameObject;
+            light = thoughtBubble.value.transform.GetChild(1).gameObject;
+            ball = thoughtBubble.value.transform.GetChild(2).gameObject;
+            zzz = thoughtBubble.value.transform.GetChild(3).gameObject;
+
+			//enable objects
+			burger.SetActive(enableBurger);
+			light.SetActive(enableLight);
+			ball.SetActive(enableBall);
+			zzz.SetActive(enableZzz);
+        }
 
 		//Called once per frame while the action is active.
 		protected override void OnUpdate() {
-			
-		}
+            bubbleTransform.value.position = agent.transform.position + Vector3.up;
+            thoughtBubble.value.transform.position = bubbleTransform.value.position;
+
+            time += Time.deltaTime;
+            if (time > 5)
+            {
+				EndAction(true);
+            }
+        }
 
 		//Called when the task is disabled.
 		protected override void OnStop() {
