fe5e0197cb17b4d27e8b40008e58abda5646e143
Giovani Vitti <giovani.mamprin@icloud.com>
2026-02-23 18:46:50 -0500

    Added sleep and photosynthesize action states

--- Animal_Assignment/Assets/Scripts/ActionTasks/PhotosynthesizeAT.cs
+++ Animal_Assignment/Assets/Scripts/ActionTasks/PhotosynthesizeAT.cs
@@ -10,6 +10,8 @@ namespace NodeCanvas.Tasks.Actions {
 
 		private NavMeshAgent navAgent;
 		public BBParameter<Transform> lightSpot;
+		public BBParameter<float> light;
+		private Light shine;
 
 		//Use for initialization. This is called only once in the lifetime of the task.
 		//Return null if init was successfull. Return an error string otherwise
@@ -23,17 +25,25 @@ namespace NodeCanvas.Tasks.Actions {
 		protected override void OnExecute() {
             navAgent = agent.GetComponent<NavMeshAgent>();
 			navAgent.SetDestination(lightSpot.value.position);
+			shine = agent.GetComponent<Light>();
         }
 
 		//Called once per frame while the action is active.
 		protected override void OnUpdate() {
 			
+			if(Vector3.Distance(agent.transform.position, lightSpot.value.position) <= 1 && light.value < 10)
+			{
+				light.value += Time.deltaTime;
+				agent.transform.Rotate(0, 1, 0);
+				shine.enabled = true;
+
+			} 
 		}
 
 		//Called when the task is disabled.
 		protected override void OnStop() {
-			
-		}
+            shine.enabled = false;
+        }
 
 		//Called when the task is paused.
 		protected override void OnPause() {
--- Animal_Assignment/Assets/Scripts/ActionTasks/SleepAT.cs
+++ Animal_Assignment/Assets/Scripts/ActionTasks/SleepAT.cs
@@ -10,6 +10,7 @@ namespace NodeCanvas.Tasks.Actions {
 
         private NavMeshAgent navAgent;
         public BBParameter<Transform> caveSpot;
+		public BBParameter<float> sleep;
         //Use for initialization. This is called only once in the lifetime of the task.
         //Return null if init was successfull. Return an error string otherwise
         protected override string OnInit() {
@@ -27,12 +28,17 @@ namespace NodeCanvas.Tasks.Actions {
 		//Called once per frame while the action is active.
 		protected override void OnUpdate() {
 			
+			if (Vector3.Distance(agent.transform.position, caveSpot.value.position) <= 0.5)
+			{
+				agent.transform.eulerAngles = new Vector3(0, 0, 180);
+				sleep.value += Time.deltaTime;
+			}
 		}
 
 		//Called when the task is disabled.
 		protected override void OnStop() {
-			
-		}
+            agent.transform.eulerAngles = Vector3.zero;
+        }
 
 		//Called when the task is paused.
 		protected override void OnPause() {
